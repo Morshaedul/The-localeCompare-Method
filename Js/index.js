1ae1411576590323da40b0026bfb1dd996508052
Morshaedul <morshaedulanirbanit@gmail.com>
2022-07-26 19:51:06 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,12 @@
+let text1 = "ab";
+let text2 = "cd";
+let result = text1.localeCompare(text2);
+
+document.getElementById("demo").innerHTML = result;
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
